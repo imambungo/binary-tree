6feb9aee7ec86635868e3e7791acd29d27f898b9
M. Imam Pratama <imambungo@gmail.com>
2018-10-25 08:47:20 +0700

    Get started

---
+++ BinaryTree.java
@@ -0,0 +1,35 @@
+class BinaryTree {
+    public Node root;
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public boolean isOneElement() {
+        if (!isEmpty()) {
+            return this.root.
+        }
+    }
+
+    class Node {
+        private int value;
+        private Node rightChild;
+        private Node leftChild;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+
+        public Node getRightChild() {
+            return this.rightChild;
+        }
+
+        public Node getLeftChild() {
+            return this.leftChild;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Make a binary tree using java.
