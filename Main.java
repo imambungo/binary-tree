617c27290c46811addb22ad7f2396044bca420cb
M. Imam Pratama <imambungo@gmail.com>
2018-11-01 09:45:18 +0700

    ngetes isSkewedLeft()

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
 class Main {
     public static void main(String[] args) {
+        // BinaryTree a = new BinaryTree();
+        // a.plant(1);
+        // a.root.setLeftChild(2);
+        // a.root.setRightChild(3);
+        // a.root.getLeftChild().setLeftChild(4);
+        // a.root.getLeftChild().setRightChild(5);
+        // a.root.getRightChild().setLeftChild(6);
+        // a.root.getRightChild().setRightChild(7);
+        // a.root.getLeftChild().getLeftChild().setLeftChild(8);
+        // a.root.getLeftChild().getLeftChild().setRightChild(9);
+        // a.printPreOrder();
+        // a.printInOrder();
+        // a.printPostOrder();
+        // a.printLeaf();
+        // a.jumlahDaun();
+        // a.depth();
+
+        // BinaryTree b = new BinaryTree();
+        // // b.plant(1);
+        // b.printPreOrder();
+        // b.printInOrder();
+        // b.printPostOrder();
+        // b.printLeaf();
+        // b.jumlahDaun();
+        // b.depth();
+
         BinaryTree a = new BinaryTree();
         a.plant(1);
         a.root.setLeftChild(2);
@@ -10,20 +36,15 @@ class Main {
         a.root.getRightChild().setRightChild(7);
         a.root.getLeftChild().getLeftChild().setLeftChild(8);
         a.root.getLeftChild().getLeftChild().setRightChild(9);
-        a.printPreOrder();
-        a.printInOrder();
-        a.printPostOrder();
-        a.printLeaf();
-        a.jumlahDaun();
-        a.depth();
+        System.out.println(a.isSkewedLeft());
 
         BinaryTree b = new BinaryTree();
-        // b.plant(1);
-        b.printPreOrder();
-        b.printInOrder();
-        b.printPostOrder();
-        b.printLeaf();
-        b.jumlahDaun();
-        b.depth();
+        b.plant(1);
+        b.root.setLeftChild(2);
+        b.root.getLeftChild().setLeftChild(3);
+        b.root.getLeftChild().getLeftChild().setLeftChild(4);
+        System.out.println(b.isSkewedLeft());
+        b.root.getLeftChild().getLeftChild().setRightChild(5);
+        System.out.println(b.isSkewedLeft());
     }
 }
