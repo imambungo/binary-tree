4283326341f9162421e43dc4539de007e04ec82f
M. Imam Pratama <imambungo@gmail.com>
2018-11-01 10:30:03 +0700

    Tes generateCompleteBinaryTree() di main

--- Main.java
+++ Main.java
@@ -26,25 +26,36 @@ class Main {
         // b.jumlahDaun();
         // b.depth();
 
-        BinaryTree a = new BinaryTree();
-        a.plant(1);
-        a.root.setLeftChild(2);
-        a.root.setRightChild(3);
-        a.root.getLeftChild().setLeftChild(4);
-        a.root.getLeftChild().setRightChild(5);
-        a.root.getRightChild().setLeftChild(6);
-        a.root.getRightChild().setRightChild(7);
-        a.root.getLeftChild().getLeftChild().setLeftChild(8);
-        a.root.getLeftChild().getLeftChild().setRightChild(9);
-        System.out.println(a.isSkewedLeft());
+        // TES SKEWED LEFT TREE
+        // BinaryTree a = new BinaryTree();
+        // a.plant(1);
+        // a.root.setLeftChild(2);
+        // a.root.setRightChild(3);
+        // a.root.getLeftChild().setLeftChild(4);
+        // a.root.getLeftChild().setRightChild(5);
+        // a.root.getRightChild().setLeftChild(6);
+        // a.root.getRightChild().setRightChild(7);
+        // a.root.getLeftChild().getLeftChild().setLeftChild(8);
+        // a.root.getLeftChild().getLeftChild().setRightChild(9);
+        // System.out.println(a.isSkewedLeft());
 
-        BinaryTree b = new BinaryTree();
-        b.plant(1);
-        b.root.setLeftChild(2);
-        b.root.getLeftChild().setLeftChild(3);
-        b.root.getLeftChild().getLeftChild().setLeftChild(4);
-        System.out.println(b.isSkewedLeft());
-        b.root.getLeftChild().getLeftChild().setRightChild(5);
-        System.out.println(b.isSkewedLeft());
+        // BinaryTree b = new BinaryTree();
+        // b.plant(1);
+        // b.root.setLeftChild(2);
+        // b.root.getLeftChild().setLeftChild(3);
+        // b.root.getLeftChild().getLeftChild().setLeftChild(4);
+        // System.out.println(b.isSkewedLeft());
+        // b.root.getLeftChild().getLeftChild().setRightChild(5);
+        // System.out.println(b.isSkewedLeft());
+
+        // TES GENERATE COMPLETE BINARY TREE
+        BinaryTree a = new BinaryTree();
+        a.generateCompleteBinaryTree(9);
+        a.printPreOrder();
+        a.printInOrder();
+        a.printPostOrder();
+        a.printLeaf();
+        a.jumlahDaun();
+        a.depth();
     }
 }
